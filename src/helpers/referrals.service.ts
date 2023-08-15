bd59f26aa3b4e647ef538226ff7748f65901384a
Luigi ~ JU <140429059+Luis-JU@users.noreply.github.com>
2023-08-16 00:01:34 +0200

    Luigi/UI (#1)
    * chore: :bento: added noise resource
    
    * chore: :thread: added page structure
    
    * style: :lipstick: added homepage styling

---
+++ src/helpers/referrals.service.ts
@@ -0,0 +1,8 @@
+export const getReferrals = async () => {
+  return [
+    'Llena tus datos en el formulario y dale click en compartir, podrás enviar el link a cualquier persona que desees referir.',
+    'Tu referido deberá ingresar al link compartido y llenar sus datos en el formulario.',
+    ' Un asesor se pondrá en contacto con la persona que referiste para brindarle la información del proyecto.',
+    'Si tu referido compra en alguno de nuestros proyectos, serás acreedor del bono de referido.'
+  ]
+}
